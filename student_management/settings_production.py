d006ece061b30607e606a8f95ca24b6677720cf9
TheCyberMayor <adeboyeamos7@gmail.com>
2025-08-17 12:05:13 +0100

    error removed

--- student_management/settings_production.py
+++ student_management/settings_production.py
@@ -37,6 +37,8 @@ AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY', default=None)
 AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME', default=None)
 AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME', default='us-east-1')
 
+STATICFILES_DIRS = []
+
 if AWS_STORAGE_BUCKET_NAME:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_DEFAULT_ACL = 'public-read'
